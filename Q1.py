198f72f855be7afef00a0965389eb8b1a290093b
rakshithays <68296486+rakshithays@users.noreply.github.com>
2020-07-23 12:01:18 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -11,3 +11,8 @@ del a
 still b refer to same memory reference 
 
 which tell variable are nothing but a reference to a memory location
+What are python variables? And assignment statement?
+Ans:variable:variables are nothing but reversed memory locations to store values. This means that when you created a variable you reverse some space in memory
+Python variables (A-Z, 0-9,_)
+Variable names are case sensitive (age, Age,AGE) are 3 different variables
+Assignment statement:name=expression. The purpose of python's assignment statement is to associate names with values in your program. It is the only statement that does not starting with a keyword. An assignment statement is a line containing at least one single equal sign(=) that is not inside parentheses.
